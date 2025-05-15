b904974f5e0491c3d23a604a3ea59c01ad1e6a04
Mahmoud alsirafy <alsirafy123@gmail.com>
2025-05-15 20:45:24 +0300

    testing livewire but not worhing

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -2,19 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Counter extends Component
 {
-    public $count = 0;
+    public $search = '';
 
-    public function increment()
-    {
-        $this->count++;
-    }
-   
+
+
     public function render()
     {
-        return view('livewire.counter');
+        return view('livewire.counter', [
+            'users' => User::where('name', $this->search)->get(),
+        ]);
     }
 }
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,5 +1,9 @@
+<div>
+    <input wire:model="search" type="text" placeholder="Search users..."/>
 
-<div style="text-align: center">
-    <button wire:click="increment">+</button>
-    <h1>{{ $count }}</h1>
+    <ul>
+        @foreach($users as $user)
+            <li>{{ $user->name }}</li>
+        @endforeach
+    </ul>
 </div>
